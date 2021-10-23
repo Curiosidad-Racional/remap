875824d522f350da7c80d5223779a0d750c3b9a4
Curiosidad-Racional <hecenra@gmail.com>
2021-10-23 14:33:06 +0200

    improved log

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-exec >> .remap.log 2>&1
-date "+%Y-%m-%d %b %H:%M"
 if [ "$1" != serio ]
 then FILT=tail
 else FILT=head
@@ -10,4 +8,6 @@ pgrep -x remap >/dev/null && pkill -x intercept
 wait
 sleep 1
 DEV="/dev/input/$(sed -nr 's/^.*sysrq kbd (event[0-9]+) leds.*$/\1/p' /proc/bus/input/devices|$FILT -1)"
+exec >> .remap.log 2>&1
+date "+%Y-%m-%d %b %H:%M"
 (intercept -g "$DEV" | ./remap -s | uinput -d "$DEV") &
