f3f74b4f629a5afe4d3f9d0601fcb67f2183055d
Curiosidad-Racional <hecenra@gmail.com>
2021-10-23 14:33:32 +0200

    fixed dirty end of macro

--- remap.c
+++ remap.c
@@ -175,6 +175,256 @@ void delete_focused_window() {
   xcb_flush(g_conn);
 }
 
+char *key_names[] = {
+    "KEY_RESERVED",
+    "KEY_ESC",
+    "KEY_1",
+    "KEY_2",
+    "KEY_3",
+    "KEY_4",
+    "KEY_5",
+    "KEY_6",
+    "KEY_7",
+    "KEY_8",
+    "KEY_9",
+    "KEY_0",
+    "KEY_MINUS",
+    "KEY_EQUAL",
+    "KEY_BACKSPACE",
+    "KEY_TAB",
+    "KEY_Q",
+    "KEY_W",
+    "KEY_E",
+    "KEY_R",
+    "KEY_T",
+    "KEY_Y",
+    "KEY_U",
+    "KEY_I",
+    "KEY_O",
+    "KEY_P",
+    "KEY_LEFTBRACE",
+    "KEY_RIGHTBRACE",
+    "KEY_ENTER",
+    "KEY_LEFTCTRL",
+    "KEY_A",
+    "KEY_S",
+    "KEY_D",
+    "KEY_F",
+    "KEY_G",
+    "KEY_H",
+    "KEY_J",
+    "KEY_K",
+    "KEY_L",
+    "KEY_SEMICOLON",
+    "KEY_APOSTROPHE",
+    "KEY_GRAVE",
+    "KEY_LEFTSHIFT",
+    "KEY_BACKSLASH",
+    "KEY_Z",
+    "KEY_X",
+    "KEY_C",
+    "KEY_V",
+    "KEY_B",
+    "KEY_N",
+    "KEY_M",
+    "KEY_COMMA",
+    "KEY_DOT",
+    "KEY_SLASH",
+    "KEY_RIGHTSHIFT",
+    "KEY_KPASTERISK",
+    "KEY_LEFTALT",
+    "KEY_SPACE",
+    "KEY_CAPSLOCK",
+    "KEY_F1",
+    "KEY_F2",
+    "KEY_F3",
+    "KEY_F4",
+    "KEY_F5",
+    "KEY_F6",
+    "KEY_F7",
+    "KEY_F8",
+    "KEY_F9",
+    "KEY_F10",
+    "KEY_NUMLOCK",
+    "KEY_SCROLLLOCK",
+    "KEY_KP7",
+    "KEY_KP8",
+    "KEY_KP9",
+    "KEY_KPMINUS",
+    "KEY_KP4",
+    "KEY_KP5",
+    "KEY_KP6",
+    "KEY_KPPLUS",
+    "KEY_KP1",
+    "KEY_KP2",
+    "KEY_KP3",
+    "KEY_KP0",
+    "KEY_KPDOT",
+    "KEY_ZENKAKUHANKAKU",
+    "KEY_102ND",
+    "KEY_F11",
+    "KEY_F12",
+    "KEY_RO",
+    "KEY_KATAKANA",
+    "KEY_HIRAGANA",
+    "KEY_HENKAN",
+    "KEY_KATAKANAHIRAGANA",
+    "KEY_MUHENKAN",
+    "KEY_KPJPCOMMA",
+    "KEY_KPENTER",
+    "KEY_RIGHTCTRL",
+    "KEY_KPSLASH",
+    "KEY_SYSRQ",
+    "KEY_RIGHTALT",
+    "KEY_LINEFEED",
+    "KEY_HOME",
+    "KEY_UP",
+    "KEY_PAGEUP",
+    "KEY_LEFT",
+    "KEY_RIGHT",
+    "KEY_END",
+    "KEY_DOWN",
+    "KEY_PAGEDOWN",
+    "KEY_INSERT",
+    "KEY_DELETE",
+    "KEY_MACRO",
+    "KEY_MUTE",
+    "KEY_VOLUMEDOWN",
+    "KEY_VOLUMEUP",
+    "KEY_POWER",
+    "KEY_KPEQUAL",
+    "KEY_KPPLUSMINUS",
+    "KEY_PAUSE",
+    "KEY_SCALE",
+    "KEY_KPCOMMA",
+    "KEY_HANGEUL",
+    "KEY_HANJA",
+    "KEY_YEN",
+    "KEY_LEFTMETA",
+    "KEY_RIGHTMETA",
+    "KEY_COMPOSE",
+    "KEY_STOP",
+    "KEY_AGAIN",
+    "KEY_PROPS",
+    "KEY_UNDO",
+    "KEY_FRONT",
+    "KEY_COPY",
+    "KEY_OPEN",
+    "KEY_PASTE",
+    "KEY_FIND",
+    "KEY_CUT",
+    "KEY_HELP",
+    "KEY_MENU",
+    "KEY_CALC",
+    "KEY_SETUP",
+    "KEY_SLEEP",
+    "KEY_WAKEUP",
+    "KEY_FILE",
+    "KEY_SENDFILE",
+    "KEY_DELETEFILE",
+    "KEY_XFER",
+    "KEY_PROG1",
+    "KEY_PROG2",
+    "KEY_WWW",
+    "KEY_MSDOS",
+    "KEY_COFFEE",
+    "KEY_ROTATE_DISPLAY",
+    "KEY_CYCLEWINDOWS",
+    "KEY_MAIL",
+    "KEY_BOOKMARKS",
+    "KEY_COMPUTER",
+    "KEY_BACK",
+    "KEY_FORWARD",
+    "KEY_CLOSECD",
+    "KEY_EJECTCD",
+    "KEY_EJECTCLOSECD",
+    "KEY_NEXTSONG",
+    "KEY_PLAYPAUSE",
+    "KEY_PREVIOUSSONG",
+    "KEY_STOPCD",
+    "KEY_RECORD",
+    "KEY_REWIND",
+    "KEY_PHONE",
+    "KEY_ISO",
+    "KEY_CONFIG",
+    "KEY_HOMEPAGE",
+    "KEY_REFRESH",
+    "KEY_EXIT",
+    "KEY_MOVE",
+    "KEY_EDIT",
+    "KEY_SCROLLUP",
+    "KEY_SCROLLDOWN",
+    "KEY_KPLEFTPAREN",
+    "KEY_KPRIGHTPAREN",
+    "KEY_NEW",
+    "KEY_REDO",
+    "KEY_F13",
+    "KEY_F14",
+    "KEY_F15",
+    "KEY_F16",
+    "KEY_F17",
+    "KEY_F18",
+    "KEY_F19",
+    "KEY_F20",
+    "KEY_F21",
+    "KEY_F22",
+    "KEY_F23",
+    "KEY_F24",
+    "unknown",
+    "unknown",
+    "unknown",
+    "unknown",
+    "unknown",
+    "KEY_PLAYCD",
+    "KEY_PAUSECD",
+    "KEY_PROG3",
+    "KEY_PROG4",
+    "KEY_DASHBOARD",
+    "KEY_SUSPEND",
+    "KEY_CLOSE",
+    "KEY_PLAY",
+    "KEY_FASTFORWARD",
+    "KEY_BASSBOOST",
+    "KEY_PRINT",
+    "KEY_HP",
+    "KEY_CAMERA",
+    "KEY_SOUND",
+    "KEY_QUESTION",
+    "KEY_EMAIL",
+    "KEY_CHAT",
+    "KEY_SEARCH",
+    "KEY_CONNECT",
+    "KEY_FINANCE",
+    "KEY_SPORT",
+    "KEY_SHOP",
+    "KEY_ALTERASE",
+    "KEY_CANCEL",
+    "KEY_BRIGHTNESSDOWN",
+    "KEY_BRIGHTNESSUP",
+    "KEY_MEDIA",
+    "KEY_SWITCHVIDEOMODE",
+    "KEY_KBDILLUMTOGGLE",
+    "KEY_KBDILLUMDOWN",
+    "KEY_KBDILLUMUP",
+    "KEY_SEND",
+    "KEY_REPLY",
+    "KEY_FORWARDMAIL",
+    "KEY_SAVE",
+    "KEY_DOCUMENTS",
+    "KEY_BATTERY",
+    "KEY_BLUETOOTH",
+    "KEY_WLAN",
+    "KEY_UWB",
+    "KEY_UNKNOWN",
+    "KEY_VIDEO_NEXT",
+    "KEY_VIDEO_PREV",
+    "KEY_BRIGHTNESS_CYCLE",
+    "KEY_BRIGHTNESS_AUTO",
+    "KEY_DISPLAY_OFF",
+    "KEY_WWAN",
+    "KEY_RFKILL",
+    "KEY_MICMUTE",
+    };
 
 unsigned short keyboard_total = 0;
 __s32 keyboard[] = {[0 ... 248] = 0};
@@ -230,7 +480,7 @@ struct input_event makro_events[] = {
     [0 ... MAKRO_MAX] = {.type = EV_KEY, .code = 0, .value = 0}};
 
 void write_event(const struct input_event event) {
-  //fprintf(stderr, "%d, %d, %d\n", event.type, event.code, event.value);
+  //fprintf(stderr, "%d, %s, %d\n", event.type, key_names[event.code], event.value);
   fwrite(&event, sizeof(event), 1, stdout);
   //fflush(stdout);
   switch (event.value) {
@@ -568,8 +818,12 @@ int main(int argc, char *argv[]) {
           case WC_TERM:
           case WC_OTHERS:
             repeat = repeat * 10 + (digit-1) % 10;
-            char text[16];
-            sprintf(text, "C-%d", repeat);
+            char text[32];
+            if (makro_recording) {
+              sprintf(text, "[RM] C-%d", repeat);
+            } else {
+              sprintf(text, "C-%d", repeat);
+            }
             show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
             continue;
           }
@@ -957,8 +1211,13 @@ int main(int argc, char *argv[]) {
           switch (check_window_classname()) {
           case WC_OTHERS:
             group_key = GK_CTRL_C;
-            char text[] = "C+C -> {T,C+C,C+D,C+E,C+0-9}";
-            show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+            if (makro_recording) {
+              char text[] = "[RM] C+C -> {T,C+C,C+D,C+E,C+0-9}";
+              show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+            } else {
+              char text[] = "C+C -> {T,C+C,C+D,C+E,C+0-9}";
+              show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+            }
             continue;
           }
         } else if (check_key2(KEY_LEFTCTRL, KEY_X)) {
@@ -966,8 +1225,13 @@ int main(int argc, char *argv[]) {
           case WC_TERM:
           case WC_OTHERS:
             group_key = GK_CTRL_X;
-            char text[] = "C+X -> {2,3,5,B,E,K,O,R,T,U,(,),S+O,C+C,C+F,C+S}";
-            show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+            if (makro_recording) {
+              char text[] = "[RM] C+X -> {2,3,5,B,E,K,O,R,T,U,(,),S+O,C+C,C+F,C+S}";
+              show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+            } else {
+              char text[] = "C+X -> {2,3,5,B,E,K,O,R,T,U,(,),S+O,C+C,C+F,C+S}";
+              show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+            }
             continue;
           }
         }
@@ -1024,13 +1288,23 @@ int main(int argc, char *argv[]) {
           continue;
         } else if (check_key1(KEY_R)) {
           group_key = GK_CTRL_X__R;
-          char text[] = "C+X R -> {M,B}";
-          show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+          if (makro_recording) {
+            char text[] = "[RM] C+X R -> {M,B}";
+            show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+          } else {
+            char text[] = "C+X R -> {M,B}";
+            show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+          }
           continue;
         } else if (check_key1(KEY_T)) {
           group_key = GK_CTRL_X__T;
-          char text[] = "C+X T -> {0,2,M,O,S-M,S-O}";
-          show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+          if (makro_recording) {
+            char text[] = "[RM] C+X T -> {0,2,M,O,S-M,S-O}";
+            show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+          } else {
+            char text[] = "C+X T -> {0,2,M,O,S-M,S-O}";
+            show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
+          }
           continue;
         }
         // MACROS
@@ -1064,21 +1338,27 @@ int main(int argc, char *argv[]) {
           makro_events_idx = 0;
           makro_recording = true;
           group_key = GK_EMACS;
-          char text[] = "RECORDING MACRO";
+          char text[] = "[RECORDING MACRO]";
           show_text_window(text, 0xffcccccc, 0xffa32cc4, g_focused_window);
           continue;
         } else if (check_key2(KEY_LEFTSHIFT, KEY_9)) {
-          makro_recording = false;
-          while (makro_events_idx && is_mod(makro_events[makro_events_idx].code))
-            --makro_events_idx;
-          if (makro_events_idx && makro_events[makro_events_idx].code == KEY_9)
-            --makro_events_idx;
-          while (makro_events_idx && is_mod(makro_events[makro_events_idx].code))
-            --makro_events_idx;
-          if (makro_events_idx && makro_events[makro_events_idx].code == KEY_X)
-            --makro_events_idx;
-          while (makro_events_idx && is_mod(makro_events[makro_events_idx].code))
-            --makro_events_idx;
+          if (makro_recording) {
+            makro_recording = false;
+            while (makro_events_idx && is_mod(makro_events[makro_events_idx - 1].code))
+              --makro_events_idx;
+            if (makro_events_idx && makro_events[makro_events_idx - 1].code == KEY_X)
+              --makro_events_idx;
+            while (makro_events_idx && makro_events[makro_events_idx - 1].code == KEY_LEFTCTRL
+                   && makro_events[makro_events_idx - 1].value == 1)
+              --makro_events_idx;
+            /* fprintf(stderr, "RECODED MACRO [\n"); */
+            /* for (unsigned short i = 0; i < makro_events_idx; ++i) */
+            /*   fprintf(stderr, "  %d, %s, %d\n", */
+            /*           makro_events[i].type, */
+            /*           key_names[makro_events[i].code], */
+            /*           makro_events[i].value); */
+            /* fprintf(stderr, "] RECORDED MACRO\n"); */
+          }
           group_key = GK_EMACS;
           show_text_window(NULL, 0, 0, g_focused_window);
           continue;
