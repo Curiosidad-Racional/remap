b5793572a1e4fbf4e05b41ca6b344a07358f4b13
Rational-Curiosity <hecenra@gmail.com>
2023-01-21 15:22:08 +0100

    scripts to launch remap

---
+++ remap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+SCRIPT="$(realpath "$0")"
+if [ -n "$SUDO_USER" ]
+then
+    cd "$(dirname "$SCRIPT")"
+    pkill -x -9 intercept
+
+    sleep 1
+    DEV="$1"
+    shift
+    (intercept -g "$DEV" | ./remap -C "feh --no-fehbg --bg-fill red.png" -c "/home/$SUDO_USER/.fehbg" "$@" | uinput -d "$DEV") &
+else
+    USBKBD="$(grep -B 4 -E 'sysrq kbd (leds )?(event[0-9]+)' /proc/bus/input/devices|grep -B 1 -E '^P: Phys=u'|grep '^N: Name='|grep -v 'DaKai 2.4G RX'|cut -d'"' -f2|tr '\n' '!')"
+    OTHKBD="$(grep -B 4 -E 'sysrq kbd (leds )?(event[0-9]+)' /proc/bus/input/devices|grep -B 1 -E '^P: Phys=[^u]'|grep '^N: Name='|cut -d'"' -f2|head -c -1|tr '\n' '!')"
+    INPUTS="$(yad --separator='"' --focus-field=3 --form --field=device:CB --field=args:CB --field=sudo:H -- "$USBKBD$OTHKBD" '-s!-s -v')"
+    if [ "$?" != 0 ]
+    then
+        return
+    fi
+    DEVICE="$(echo "$INPUTS"|cut -d'"' -f1)"
+    PARAMS="$(echo "$INPUTS"|cut -d'"' -f2)"
+    PASSWD="$(echo "$INPUTS"|cut -d'"' -f3)"
+
+    DEVICE="/dev/input/$(grep -A 4 "$DEVICE" /proc/bus/input/devices|sed -nr 's/^.*sysrq kbd (leds )?(event[0-9]+).*$/\2/p'|head -1)"
+    if [ ! -c "$DEVICE" ]
+    then
+        yad --button=Ok --text="<span foreground=\"red\">Invalid device:</span> $DEVICE"
+        return
+    fi
+    if [ -z "$PASSWD" ]
+    then
+        yad --button=Ok --text='<span foreground="red">Empty password</span>'
+        return
+    fi
+    if echo "$PASSWD" | sudo -S "$SCRIPT" "$DEVICE" $PARAMS
+    then
+        yad --button=Ok --text='Remap ready'
+    else
+        yad --button=Ok --text='<span foreground="red">Remap failed</span>'
+    fi
+fi
--- run.sh
+++ run.sh
@@ -17,15 +17,16 @@ done
 PID=$(pgrep -x remap)
 if [ -n "$PID" ]
 then
-    pkill -x intercept
-    kill "$PID"
+    pkill -x -9 intercept
+    kill -9 "$PID"
 fi
+sleep 1
 DEV="/dev/input/$(sed -nr '/leds/{s/^.*sysrq kbd (leds )?(event[0-9]+).*$/\2/p}' /proc/bus/input/devices|$FILT -1)"
 exec > .remap.log 2>&1
-sleep 1
+SCRIPT_PATH="$(dirname $0)"
 if [ -n "$MODE" ]
 then
-    (intercept -g "$DEV" | ./remap -s "$MODE" | uinput -d "$DEV") &
+    (intercept -g "$DEV" | ./remap -C "feh --no-fehbg --bg-fill $SCRIPT_PATH/red.png" -c "/home/$SUDO_USER/.fehbg" -s "$MODE" | uinput -d "$DEV") &
 else
-    (intercept -g "$DEV" | ./remap -s | uinput -d "$DEV") &
+    (intercept -g "$DEV" | ./remap -C "feh --no-fehbg --bg-fill $SCRIPT_PATH/red.png" -c "/home/$SUDO_USER/.fehbg" -s | uinput -d "$DEV") &
 fi
